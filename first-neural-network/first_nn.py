16f984208538af59b86dda370ac4fc968cffb0da
fqihangf <fqihang@gmail.com>
2017-11-28 16:53:54 +0800

    add trainning code

--- first-neural-network/first_nn.py
+++ first-neural-network/first_nn.py
@@ -356,62 +356,46 @@ unittest.TextTestRunner().run(suite)
 
 # In[16]:
 
-import sys
-
-### Set the hyperparameters here ###
-iterations = 100
-learning_rate = 0.1
-hidden_nodes = 2
-output_nodes = 1
-
-N_i = train_features.shape[1]
-network = NeuralNetwork(N_i, hidden_nodes, output_nodes, learning_rate)
-
-losses = {'train':[], 'validation':[]}
-for ii in range(iterations):
-    # Go through a random batch of 128 records from the training data set
-    batch = np.random.choice(train_features.index, size=128)
-    X, y = train_features.ix[batch].values, train_targets.ix[batch]['cnt']
-                             
-    network.train(X, y)
-    
-    # Printing out the training progress
-    train_loss = MSE(network.run(train_features).T, train_targets['cnt'].values)
-    val_loss = MSE(network.run(val_features).T, val_targets['cnt'].values)
-    sys.stdout.write("\rProgress: {:2.1f}".format(100 * ii/float(iterations))                      + "% ... Training loss: " + str(train_loss)[:5]                      + " ... Validation loss: " + str(val_loss)[:5])
-    sys.stdout.flush()
-    
-    losses['train'].append(train_loss)
-    losses['validation'].append(val_loss)
+#input_nodes, hidden_nodes, output_nodes, learning_rate
+hidden_nodes_cnt = 8;
+bpnn = NeuralNetwork(train_features.shape[1],hidden_nodes_cnt,train_targets.shape[1],0.5)
+#bpnn.train(train_features,train_targets)
+#print(train_features.shape)
+#print(val_targets.shape)
+#print(type(train_features))
+#print(train_targets.shape)
+train_features_array = np.array(train_features)
+train_tags_array = np.array(train_targets)
 
 
-# In[17]:
+val_features_array = np.array(val_features)
+val_targets_array = np.array(val_targets)
 
-plt.plot(losses['train'], label='Training loss')
-plt.plot(losses['validation'], label='Validation loss')
-plt.legend()
-_ = plt.ylim()
+#print(type(train_features_array))
+#sgd
+minibatch = 600
+
+
+#every validate_error_step validate the nn with mse if the error small enough quit the train
+validate_error_step  = 5 
+epoch = 10000
+for i in range(epoch):
+    idx = np.random.randint(train_features_array.shape[0], size=minibatch)
+    batch_train_feature_array = train_features_array[idx, :]
+    batch_train_feature_tag  = train_tags_array[idx, :]       
+
+    bpnn.train(batch_train_feature_array,batch_train_feature_tag)
+
+    if (i % validate_error_step) == 0:
+        mse = bpnn.val_nn(val_features_array,val_targets_array)
+        print(mse)
+        #early stop
+        if mse.mean() < 0.02 :
+            break
 
 
 # 检查预测结果
 # 使用测试数据看看网络对数据建模的效果如何。如果完全错了，请确保网络中的每步都正确实现
-
-# In[18]:
-
-fig, ax = plt.subplots(figsize=(8,4))
-
-mean, std = scaled_features['cnt']
-predictions = network.run(test_features).T*std + mean
-ax.plot(predictions[0], label='Prediction')
-ax.plot((test_targets['cnt']*std + mean).values, label='Data')
-ax.set_xlim(right=len(predictions))
-ax.legend()
-
-dates = pd.to_datetime(rides.ix[test_data.index]['dteday'])
-dates = dates.apply(lambda d: d.strftime('%b %d'))
-ax.set_xticks(np.arange(len(dates))[12::24])
-_ = ax.set_xticklabels(dates[12::24], rotation=45)
-
 
 # *可选：思考下你的结果（我们不会评估这道题的答案）
 # 请针对你的结果回答以下问题。模型对数据的预测效果如何？哪里出现问题了？为何出现问题呢？
